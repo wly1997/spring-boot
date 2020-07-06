c82468518953f59eb282baaa70577385abb839b2
wly <1067904312@qq.com>
2020-07-06 17:18:02 +0800

    activemq topic

---
+++ day08_activemq_springboot_topic/src/main/java/com/java/activemq/Day08ActivemqSpringbootTopicApplication.java
@@ -0,0 +1,13 @@
+package com.java.activemq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Day08ActivemqSpringbootTopicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Day08ActivemqSpringbootTopicApplication.class, args);
+	}
+
+}
---
+++ day08_activemq_springboot_topic/src/test/java/com/java/activemq/Day08ActivemqSpringbootTopicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.java.activemq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Day08ActivemqSpringbootTopicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
